dc3d66b7f6b8ea2c70ce4369a49e582169b4a1df
josephines1 <josephine.bless777@gmail.com>
2024-03-05 11:58:40 +0700

    Menambahkan button dan textView

--- app/src/main/java/com/example/aplikasipertama/MainActivity.kt
+++ app/src/main/java/com/example/aplikasipertama/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.aplikasipertama
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val textView = findViewById<TextView>(R.id.tv_hello)
+        val button = findViewById<Button>(R.id.btn_tombol)
+
+        button.setOnClickListener {
+            textView.text = "Ohayou"
         }
     }
 }
